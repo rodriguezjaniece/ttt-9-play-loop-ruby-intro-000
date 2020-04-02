240e61100da1c927a8437776ceff913a853edd41
Janiece Rodriguez <rodriguezjaniece@yahoo.com>
2020-04-02 17:03:09 +0000

    Automatically backed up by Learn

--- lib/play.rb
+++ lib/play.rb
@@ -37,8 +37,9 @@ end
 
 # Define your play method below
 def play(board)
-  turn(board) = 0
-while turn(board) < 9
-  turn(board) += 1
+  turn = 0
+while turn ==  9
+  turn += 1
+  turn(board)
 end
 end
